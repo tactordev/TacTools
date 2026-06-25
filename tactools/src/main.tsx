cb07b0b853635a53ba38df746c6b78737f89863c
Tactor <119103845+tactordev@users.noreply.github.com>
2026-06-25 13:47:18 +0100

    Main setup

---
+++ tactools/src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import Sidebar from "./components/sidebar";
+import TitleBar from "./components/title-bar";
+import "./globals.css";
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <main className="flex flex-col w-full h-[100vh]">
+      <TitleBar />
+      <Sidebar />
+    </main>
+);
