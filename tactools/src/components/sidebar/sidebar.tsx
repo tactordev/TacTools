fd70ce75dd2a3f861f03ffd23aba0c82b3449df0
Tactor <119103845+tactordev@users.noreply.github.com>
2026-06-28 17:53:22 +0100

    Use new buttons

--- tactools/src/components/sidebar/sidebar.tsx
+++ tactools/src/components/sidebar/sidebar.tsx
@@ -8,7 +8,8 @@ import {
     MonitorCog,
     BadgeAlert
 } from "lucide-react";
-
+import Notes from "./notes";
+import Button from "../utils/button";
 
 
 export default function Sidebar() {
@@ -18,18 +19,18 @@ export default function Sidebar() {
     return (
         <div className="flex flex-col items-center bg-[#EDEDF2]/40 mt-0.5 w-64 h-full transition-all duration-200 shadow-sm z-20">
             <div className="flex flex-row gap-2 mt-2">
-                <div className={`${state === "notes" ? "bg-[#EDEDF2]" : "bg-[#EDEDF2]/40"} group hover:bg-[#EDEDF2]/80 transition-all duration-200 hover:cursor-pointer px-2 py-1 rounded-md shadow-xs`} title="Notes" onClick={() => setState("notes")}><Notebook className={`${state === "notes" ? "text-gray-500" : "text-gray-400/60 group-hover:text-gray-400"} w-4 h-4`}  xlinkTitle="Notes" onClick={() => setState("notes")}/></div>
-                <div className={`${state === "planning" ? "bg-[#EDEDF2]" : "bg-[#EDEDF2]/40"} group hover:bg-[#EDEDF2]/80 transition-all duration-200 hover:cursor-pointer px-2 py-1 rounded-md shadow-xs`} title="Calendar" onClick={() => setState("planning")}><Calendar className={`${state === "planning" ? "text-gray-500" : "text-gray-400/60 group-hover:text-gray-400"} w-4 h-4 transition-all duration-200`} xlinkTitle="Calendar" onClick={() => setState("planning")} /></div>
-                <div className={`${state === "utilities" ? "bg-[#EDEDF2]" : "bg-[#EDEDF2]/40"} group hover:bg-[#EDEDF2]/80 transition-all duration-200 hover:cursor-pointer px-2 py-1 rounded-md shadow-xs`} title="Utilities" onClick={() => setState("utilities")}><MonitorCog className={`${state === "utilities" ? "text-gray-500" : "text-gray-400/60 group-hover:text-gray-400"} w-4 h-4 transition-all duration-200`} xlinkTitle="Utilities" onClick={() => setState("utilities")} /></div>
+                <Button className={`${state === "notes" ? "bg-blue-300/20" : "bg-[#EDEDF2] text-gray-600"}`} name="Notes" onClick={() => setState("notes")}><Notebook className={`${state === "notes" ? "text-blue-400/90" : "text-gray-400 group-hover:text-gray-400" } w-4 h-4`}  xlinkTitle="Notes" onClick={() => setState("notes")}/></Button>
+                <Button className={`${state === "planning" ? "bg-blue-300/20" : "bg-[#EDEDF2] text-gray-600"}`} name="Calendar" onClick={() => setState("planning")}><Calendar className={`${state === "planning" ? "text-blue-400/90" : "text-gray-400 group-hover:text-gray-400" } w-4 h-4 transition-all duration-200`} xlinkTitle="Calendar" onClick={() => setState("planning")} /></Button>
+                <Button className={`${state === "utilities" ? "bg-blue-300/20" : "bg-[#EDEDF2] text-gray-600"}`} name="Utilities" onClick={() => setState("utilities")}><MonitorCog className={`${state === "utilities" ? "text-blue-400/90" : "text-gray-400 group-hover:text-gray-400" } w-4 h-4 transition-all duration-200`} xlinkTitle="Utilities" onClick={() => setState("utilities")} /></Button>
             </div>
             {
                 state === "none" ? 
                     <div className="w-full h-full flex flex-col gap-2 items-center mt-12">
-                        <BadgeAlert className="w-8 h-8 text-gray-500" />
-                        <p className="text-lg font-semibold text-gray-500">No option chosen.</p>
+                        <BadgeAlert className="w-8 h-8 text-gray-500/60" />
+                        <p className="text-lg font-semibold text-gray-500/60">No option chosen.</p>
                     </div>
                 : state === "notes" ? 
-                    <div></div>
+                    <Notes />
                 : state === "planning" ? 
                     <div></div>
                 :
