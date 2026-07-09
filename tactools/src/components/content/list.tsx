b9da39d297b560c3c178d789c38b5a9cca4e82a9
Tactor <119103845+tactordev@users.noreply.github.com>
2026-07-09 21:13:50 +0100

    Setup for list view

--- tactools/src/components/content/list.tsx
+++ tactools/src/components/content/list.tsx
@@ -1,13 +1,44 @@
+"use client";
+import { Tab } from "../../main";
+import { useEffect, useState } from "react";
 
 
-
-
-export default function List() {
-    return (
-        <div>
-            <p>
-                lisst
-            </p>
-        </div>
-    )
+type Task = {
+    id: number;
+    name: string;
+    description?: string;
+    links?: string[];
+    recurring?: Date;
+    due?: Date;
+    subTasks?: Task[]
+    parentTask?: Task;
 }
+
+type ListInfo = {
+    tasks: Task[];
+}
+
+type List = {
+    title: string;
+    storName: string;
+    info: ListInfo;
+}
+
+export default function List({ tab }: { tab: Tab; }) {
+    if (
+        tab.title === "Overview"
+    ) return ( <p> Overview section not finished. </p> );
+
+
+    const [listInfo, setListInfo] = useState<List | null>(null);
+
+
+    return (
+        <div className="mx-12 my-4 ">
+            <p className="text-gray-600 text-2xl font-semibold">Tasks</p>
+            {
+
+            }
+        </div>
+    );
+};
