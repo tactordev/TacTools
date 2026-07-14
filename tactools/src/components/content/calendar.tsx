7e6f4eeb07948715b0f856f5336c1edc473103a9
Tactor <119103845+tactordev@users.noreply.github.com>
2026-07-14 19:35:06 +0100

    Calendar base UI

--- tactools/src/components/content/calendar.tsx
+++ tactools/src/components/content/calendar.tsx
@@ -1,11 +1,83 @@
+"use client";
 
 
+import { AnimatePresence } from "framer-motion";
+import Button from "../utils/button";
+import ContextMenu from "../utils/context-menu";
+import {
+    ImportIcon
+} from "lucide-react";
+import { useState } from "react";
+
+function Day(
+    {
+        type
+    }: {
+        type: string;
+    }
+) {
+    return (
+        <div className="flex flex-col w-full items-center first:border-l-2 border-r-2 border-gray-100 h-full overflow-hidden">
+            <p className="w-full text-center font-semibold text-gray-600 h-6">
+                {
+                    type
+                }
+            </p>
+            <hr className="bg-gray-100 text-gray-100 w-full h-0.5 mt-0.5" />
+        </div>
+    );
+};
+
+function TimeLine() {
+    const times = [...Array(25).keys()];
+    return (
+        <div className="flex flex-col h-[calc(100%-4rem)] w-fit mt-3 justify-between">
+            {
+                times.map((time, index) => <p key={index} className="text-gray-600/40 flex justify-center text-xs font-mono tabular-nums">{ time }</p>)
+            }
+        </div>
+    )
+}
 
 
 export default function Calendar() {
+
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    const Import = () => {
+        const [importMenu, setImportMenu] = useState<{ x: number; y: number; } | false>(false);
+        
+        return (
+            <Button onClick={(e) => { setImportMenu({ x: e.clientX + (e.currentTarget.getBoundingClientRect().x - e.clientX), y: e.clientY - (e.clientY - e.currentTarget.getBoundingClientRect().bottom) + 5 }); }} onBlur={() => { setImportMenu(false); }} className="relative">
+                <ImportIcon className="w-4 h-4 text-gray-600" />
+                <AnimatePresence>
+                    {
+                        importMenu && (
+                            <ContextMenu x={importMenu.x} y={importMenu.y} onBlur={() => { setImportMenu(false); }}>
+                                <p>
+                                    Import calendars
+                                </p>
+                            </ContextMenu>
+                        )
+                    }
+                </AnimatePresence>
+            </Button>
+        );
+    };
+
     return (
-        <div>
-            
+        <div className="flex flex-row w-full h-full gap-0.5 px-4 my-4">
+            <div className="flex flex-col h-full pr-2 items-center">
+                <Import />
+                <TimeLine />
+            </div>
+            <div className="flex flex-row w-full justify-between">
+                {
+                    days.map((day, index) => 
+                        <Day key={index} type={day} />
+                    )
+                }
+            </div>
         </div>
     )
 }
--- tactools/src/components/utils/button.tsx
+++ tactools/src/components/utils/button.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 
 
 
-export default function Button({ children, className, name, onClick, disableMovement }: { children?: React.ReactNode; className?: string; name?: string; onClick?: (e: React.MouseEvent) => void; disableMovement?: boolean }) {
+export default function Button({ children, className, name, onClick, onBlur, disableMovement }: { children?: React.ReactNode; className?: string; name?: string; onClick?: (e: React.MouseEvent) => void; onBlur?: () => void; disableMovement?: boolean }) {
     const foo = () => { return true; }
     const button = useRef<HTMLDivElement | null>(null);
 
@@ -46,7 +46,7 @@ export default function Button({ children, className, name, onClick, disableMove
 
 
     return (
-        <div ref={button} className={`group hover:cursor-pointer hover:bg-blue-200/30 scale-100 hover:scale-105 px-2 py-1 rounded-md shadow-sm transition-all duration-200 select-none ${className}`}  onClick={onClick ?? foo } title={name ?? "Button"} onMouseDown={ handleMouseDown } onMouseUp={ handleMouseUp } onMouseLeave={handleMouseLeave}>
+        <div ref={button} className={`group hover:cursor-pointer hover:bg-blue-200/30 scale-100 hover:scale-105 px-2 py-1 rounded-md shadow-sm transition-all duration-200 select-none ${className}`}  onClick={onClick ?? foo } onBlur={onBlur ?? foo} title={name ?? "Button"} onMouseDown={ handleMouseDown } onMouseUp={ handleMouseUp } onMouseLeave={handleMouseLeave}>
             { children }
         </div>
     )
